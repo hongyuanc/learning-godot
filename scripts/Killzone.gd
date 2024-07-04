2c60576100c6b29093cc25adee6996326bfbab1d
Hong <hong.cao0824@gmail.com>
2024-07-04 15:54:00 +0800

    added support for animations and mechanics

--- scripts/Killzone.gd
+++ scripts/Killzone.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var player = $"../Player"
 
+func _ready():
+	print("Player node: ", player)
 
 func _on_body_entered(body):
 	print("You died")
+	Engine.time_scale = 0.9
+	body.get_node("CollisionShape2D").queue_free()
+	
 	timer.start()
 	
 
-
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
